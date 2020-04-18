eb70a5db7a6575c7dca10d206e82f4936988d068
ThirstyPoochDesktop <thirstypooch@protonmail.com>
2020-04-18 15:51:26 -0500

    seo, file based routing & errors

---
+++ pages/event/create.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Create an event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can create all the kinky shit you like'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
